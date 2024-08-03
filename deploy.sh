b29f8524603e0db488c68a34dcf4cde1a9aa364e
Ros Pavlov <avgust13@gmail.com>
2024-08-03 12:50:55 +0300

    test build

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd /var/pavloff-dev
-git pull origin main
-yarn install
-yarn build
+# cd /var/pavloff-dev
+# git pull origin main
+# yarn install
+# yarn build
 pm2 restart pavloff-dev
