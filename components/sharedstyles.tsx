1beddc37f1fdabaa3059bb06627db9a49e387323
Ros Pavlov <avgust13@gmail.com>
2024-08-08 15:19:20 +0300

    fixed page alignment

--- components/sharedstyles.tsx
+++ components/sharedstyles.tsx
@@ -1,21 +1,16 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-  padding: 0 0.5rem;
+  padding: 0;
+  margin: 0;
   display: flex;
   flex-flow: column nowrap;
-  justify-content: center;
   align-items: center;
   height: 100vh;
   min-height: 100vh;
 `;
 const Main = styled.main`
-  padding: 5rem 0;
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  max-width: 1100px;
 `;
 
 const Title = styled.h1`
