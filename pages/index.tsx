c943ed1c986c7e48221555903076bfa98f2a4c4d
Ros Pavlov <avgust13@gmail.com>
2024-08-08 13:16:14 +0300

    added components to page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,36 @@
 import Head from "next/head";
-import {
-  Container,
-  Main,
-  Title,
-  Description,
-  CodeTag,
-} from "@/components/sharedstyles";
-import Cards from "@/components/cards";
+import { Container, Main } from "@/components/sharedstyles";
+
+import Header from "@/components/layout/header";
+import Footer from "@/components/layout/footer";
+
+import HeroSection from "@/components/sections/hero";
+import AboutSection from "@/components/sections/about";
+import ExperienceSection from "@/components/sections/experience";
+import CertificationSection from "@/components/sections/certification";
 
 export default function Home() {
   return (
     <Container>
       <Head>
         <title>Ros - Software Engineer - IT Development Services</title>
-        <meta name="description" content="Ros - Software Engineer - IT Development Services" />
+        <meta
+          name="description"
+          content="Ros - Software Engineer - IT Development Services"
+        />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700;800&display=swap"
+        />
       </Head>
       <Main>
-        <Title>
-          - I'm Ros - Software Developer -
-        </Title>
-
-        {/* <Description>
-          Get started by editing
-          <CodeTag>pages/index.tsx</CodeTag>
-        </Description> */}
-
-        {/* <Cards /> */}
+        <Header />
+        {/* <HeroSection /> */}
+        <AboutSection />
+        {/* <ExperienceSection /> */}
+        <CertificationSection />
+        <Footer />
       </Main>
     </Container>
   );
