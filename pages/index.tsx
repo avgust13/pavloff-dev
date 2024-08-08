ffdf0e10918f5857d394fdb18f5eee661218e4eb
Ros Pavlov <avgust13@gmail.com>
2024-08-08 15:15:23 +0300

    added new font

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,6 +23,10 @@ export default function Home() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap"
         />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+        />
       </Head>
       <Main>
         <Header />
