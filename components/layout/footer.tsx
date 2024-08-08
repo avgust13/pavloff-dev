0b5a0ea862625a77d1ffc910bf57b939c3a24dab
Ros Pavlov <avgust13@gmail.com>
2024-08-08 13:12:10 +0300

    feat: Add footer component
    This commit adds a new footer component to the project. The footer is styled using styled-components and includes a copyright notice. This addition improves the overall layout and provides a consistent design across the application.

---
+++ components/layout/footer.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const FooterStyled = styled.div`
+  padding: 20px 0 10px 0;
+  text-align: center;
+  font-size: 14px;
+  color: ${({ theme }) => theme.colors.tertiary};
+`;
+
+const Footer = () => {
+  return (
+    <FooterStyled>&copy; pavloff.dev {new Date().getFullYear()}</FooterStyled>
+  );
+};
+
+export default Footer;
