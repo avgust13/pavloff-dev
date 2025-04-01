b58453b94f6c45e00d445fb5b7e03c17cf4cc3ae
Ros Pavlov <avgust13@users.noreply.github.com>
2025-04-01 15:48:59 +0800

    fix: uncomment CertificateBlock in CertificationSection component

--- components/sections/certification/index.tsx
+++ components/sections/certification/index.tsx
@@ -19,7 +19,7 @@ const CertificationSection = () => {
     <CertificationSectionStyled id="certifications">
       <Title name="certifications" />
       <CertificationContainerStyled>sss
-        {/* <CertificateBlock name="Deep Learning with Tensorflow" month="Apr" year="2024" />
+        <CertificateBlock name="Deep Learning with Tensorflow" month="Apr" year="2024" />
         <CertificateBlock name="Deep Neural Networks with PyTorch" month="Apr" year="2024" />
         <CertificateBlock name="IBM AI Engineering Specialization" month="Apr" year="2024" />
         <CertificateBlock name="Introduction to Computer Vision and Image Processing" month="Mar" year="2024" />
@@ -29,7 +29,7 @@ const CertificationSection = () => {
         <CertificateBlock name="MCPS: Microsoft Certified Professional" month="Mar" year="2014" />
         <CertificateBlock name="MS: Programming in HTML5 with JavaScript and CSS3" month="Mar" year="2014" />
         <CertificateBlock name="MCPD: ASP.NET Developer 3.5" month="Mar" year="2010" />
-        <CertificateBlock name="MCTS: .NET Framework 3.5, ASP.NET Applications" month="Dec" year="2009" /> */}
+        <CertificateBlock name="MCTS: .NET Framework 3.5, ASP.NET Applications" month="Dec" year="2009" />
       </CertificationContainerStyled>
     </CertificationSectionStyled>
   );
